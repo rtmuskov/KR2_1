5f442c6d8706858b0afcd250debbb4fe540e686e
Ghost2021 <rtmuskov@gmail.com>
2022-12-28 03:06:29 +0300

    4

---
+++ cmake-build-debug/CMakeFiles/KR2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rtmus/CLionProjects/KR2.2/main.c" "CMakeFiles/KR2.dir/main.c.obj" "gcc" "CMakeFiles/KR2.dir/main.c.obj.d"
+  "C:/Users/rtmus/CLionProjects/KR2.2/process.c" "CMakeFiles/KR2.dir/process.c.obj" "gcc" "CMakeFiles/KR2.dir/process.c.obj.d"
+  "C:/Users/rtmus/CLionProjects/KR2.2/process2.c" "CMakeFiles/KR2.dir/process2.c.obj" "gcc" "CMakeFiles/KR2.dir/process2.c.obj.d"
+  "C:/Users/rtmus/CLionProjects/KR2.2/process3.c" "CMakeFiles/KR2.dir/process3.c.obj" "gcc" "CMakeFiles/KR2.dir/process3.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/KR2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KR2.dir/main.c.obj"
+  "CMakeFiles/KR2.dir/main.c.obj.d"
+  "CMakeFiles/KR2.dir/process.c.obj"
+  "CMakeFiles/KR2.dir/process.c.obj.d"
+  "CMakeFiles/KR2.dir/process2.c.obj"
+  "CMakeFiles/KR2.dir/process2.c.obj.d"
+  "CMakeFiles/KR2.dir/process3.c.obj"
+  "CMakeFiles/KR2.dir/process3.c.obj.d"
+  "KR2.exe"
+  "KR2.exe.manifest"
+  "KR2.pdb"
+  "libKR2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KR2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
