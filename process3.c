d11bf7340d41cdba06bc6ba9b9e97fe2fdf9c1a1
Ghost2021 <rtmuskov@gmail.com>
2022-12-28 04:45:14 +0300

    6

--- process3.c
+++ process3.c
@@ -1,24 +1,23 @@
-//
-// Created by rtmus on 28.12.2022.
-//
-
 #include "process3.h"
 #include <stdlib.h>
-            void process3(int **array, int *size) {
-                int col = *size;
-                if (col != 0) {
-                    for (int i = 0; i < col; i++) {
-                        if (((*array)[i] < 0) || ((*array)[i] = (*array)[i - 1])) {
-                            col--;
-                            for (int k = i; k < col-1; k++) {
-                                (*array)[k] = (*array)[k + 1];
-                            }
-                            (*array) = realloc(*array, col * sizeof(int));
-
-                        }
-
-
+void process3(int **array, int *size) {
+    int col = *size;
+    int j=0;
+    if (col != 0) {
+        for (int i = 0; i < col; i++) {
+            for (j = i + 1; j < col; j++) {
+                if ((*array)[i] == (*array)[j]) {
+                    col--;
+                    for (int k = j; k < col - 1; k++) {
+                        (*array)[k] = (*array)[k + 1];
                     }
+                    (*array) = realloc(*array, col * sizeof(int));
+                    i--;
+                    j--;
                 }
-                *size = col;
             }
+
+        }
+    }
+    *size = col;
+}
