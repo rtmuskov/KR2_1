15f2745d38800d960a24edafa7340e9a4480cc25
Ghost2021 <rtmuskov@gmail.com>
2022-12-28 03:06:29 +0300

    5

--- process.c
+++ process.c
@@ -1,7 +1,3 @@
-//
-// Created by evgen on 28.12.2022.
-//
-
 #include "process.h"
 #include <stdlib.h>
 
@@ -10,21 +6,19 @@ void process(int **array, int *size) {
     if (col != 0) {
         for (int i = 0; i < col; i++) {
             if ((*array)[i] < 0) {
-                col--;
-                for (int k = i; k < col; k++) {
+                for (int k = i; k < col - 1; k++) {
                     (*array)[k] = (*array)[k + 1];
                 }
-                (*array) = realloc(*array, col* sizeof(int));
-
+                (*array) = realloc(*array, (col - 1)* sizeof(int));
+                col--;
+                i--;
             }
-            if ((*array)[i] > 0){
-                col ++;
-                (*array) = realloc(*array, col* sizeof(int));
-                for (int k = col-1; k > i+1; k--){
+            else if ((*array)[i] > 0){
+                (*array) = realloc(*array, (col + 1)* sizeof(int));
+                for (int k = col; k > i; k--){
                     (*array)[k] = (*array)[k - 1];
                 }
-
-                (*array)[i+1] = (*array)[i];
+                col++;
                 i++;
             }
         }
